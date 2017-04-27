49a2e5296669f836a26ebf51c3cd05f06ef79ed7
tojo13 <thomasej2000@hotmail.com>
2017-04-27 16:58:36 -0700

    experimenting with Radium styling

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
+import Radium from 'radium'
+
+const style = {
+  base: {
+    color: 'white',
+    background: '#121F1F',
+    width: '100%',
+    height: '12%',
+    position: 'absolute',
+    top: '0%',
+    left: '0%'
+  }
+}
 
 class Header extends Component {
   render () {
     return (
-      <h1 className='Header'>
-        Reporting {this.props.screenName}
+      <h1 className='Header' style={style.base}>
+        <div>Reporting {this.props.screenName}</div>
         <Link to='/'>Home</Link>
         <Link to='/graph'>Graph</Link>
         <Link to='/table'>Table</Link>
@@ -14,4 +27,4 @@ class Header extends Component {
   }
 }
 
-export default Header
+export default Radium(Header)
--- client/src/screens/home.js
+++ client/src/screens/home.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import Radium from 'radium'
 import Header from '../components/header'
+
+const style = {
+  list: {
+    color: 'blue',
+    position: 'absolute',
+    top: '30%',
+    left: '40%'
+  }
+}
 
 class Home extends Component {
   render () {
     return (
       <div className='Home'>
         <Header screenName={'Home'} />
+        <ul style={style.list}>
+          <li><Link to='/graph'>Graph</Link></li>
+          <li><Link to='/table'>Table</Link></li>
+        </ul>
       </div>
     )
   }
 }
 
-export default Home
+export default Radium(Home)
