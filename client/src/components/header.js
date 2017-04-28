ec4b2831acb1cb1d9b55be6884e3cc5c43a8fed2
tojo13 <thomasej2000@hotmail.com>
2017-04-27 17:26:32 -0700

    add Radium Grid; more style experimentation

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -4,13 +4,23 @@ import Radium from 'radium'
 
 const style = {
   base: {
-    color: 'white',
     background: '#121F1F',
     width: '100%',
     height: '12%',
     position: 'absolute',
+    marginTop: '0%',
     top: '0%',
     left: '0%'
+  },
+  title: {
+    fontFamily: 'Arimo',
+    color: 'green',
+    left: '10%',
+    position: 'absolue'
+  },
+  navLinks: {
+    fontFamily: 'Arimo',
+    color: 'red'
   }
 }
 
@@ -18,10 +28,12 @@ class Header extends Component {
   render () {
     return (
       <h1 className='Header' style={style.base}>
-        <div>Reporting {this.props.screenName}</div>
-        <Link to='/'>Home</Link>
-        <Link to='/graph'>Graph</Link>
-        <Link to='/table'>Table</Link>
+        <div style={style.title}>Reporting {this.props.screenName}</div>
+        <div style={style.navLinks}>
+          <Link to='/'>Home</Link>
+          <Link to='/graph'>Graph</Link>
+          <Link to='/table'>Table</Link>
+        </div>
       </h1>
     )
   }
